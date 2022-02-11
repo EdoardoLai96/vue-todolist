6f6e152a5a7328394fe7a8093417a077a1507356
Edoardo Lai <edoardolai96@gmail.com>
2022-02-11 17:53:33 +0100

    parte sbarrata

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 const app = new Vue({
     el: '#app',
     data: {
-        newTodo : "",
-        todoList : ["mangiare", "dormire", "leggere documentazione vue", "sport"]
+        newTodo : [""],
+        todoList : [
+            {
+                text : "mangiare",
+                done : true
+            },
+            {
+                text : "studiare",
+                done : true
+            },
+            {
+                text : "dormire",
+                done : true
+            },
+            {
+                text : "vedere la documentazione",
+                done : true
+            },
+        ],
     },
     methods: {
         addTodo(){
            this.todoList.push(this.newTodo)
+        },
+        removeTodo(index){
+            console.log(this.todoList, index)
+            this.todoList.splice(index,1)
+        },
+        prova(){
+            this.todoList.done = !this.todoList.done
+            console.log(this.todoList.done);
         }
     }
 })
